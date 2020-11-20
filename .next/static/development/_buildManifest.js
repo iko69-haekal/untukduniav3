b0691ba27344699b1cbb78f5c6a49b1738a660e7
iko69 <ikobagas69@gmail.com>
2020-11-20 21:02:22 +0700

    ubah gitignore

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/admin/jumbotron":["static\u002Fchunks\u002Fpages\u002Fadmin\u002Fjumbotron.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fadmin\u002Fjumbotron"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
